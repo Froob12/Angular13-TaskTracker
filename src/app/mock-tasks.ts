ffa7b4b98f11ad77edbec1b2d3d374b36b7c63aa
Froob12 <michaeltoladenis@gmail.com>
2021-12-12 19:50:38 -0500

    Task item component

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'October 5th at 5:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at hairdresser',
+    day: 'May 6th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Shopping',
+    day: 'May 15th at 17:30pm',
+    reminder: false,
+  },
+];
